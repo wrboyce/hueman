e52e038e08d3235f7c15b035a13040d74373caa0
Will Boyce <me@willboyce.com>
2013-03-20 08:45:33 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+
+from setuptools import setup
+
+import hueman
+
+
+requires = ['requests', 'PyYAML']
+if sys.version_info < (2, 7):
+    requires.append('argparse')
+
+setup(
+    name='hueman',
+    version=hueman.__version__,
+    description='A human interface for managing your hues.',
+    author='Will Boyce',
+    author_email='me@willboyce.com',
+    url='https://www.github.com/wrboyce/hueman',
+    license='BSD',
+    install_requires=requires,
+    packages=['hueman'],
+    entry_points={'console_scripts': {'hueman = hueman:cli'}}
+)
